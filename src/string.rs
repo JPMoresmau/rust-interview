754d79fb5ba4921fade4e6e7e2add5d4286e30cd
JP Moresmau <jp@moresmau.fr>
2019-06-10 15:33:35 +0200

    permutations

--- src/string.rs
+++ src/string.rs
@@ -1,6 +1,8 @@
 //! String functions
 
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 /// are the two string anagrams of each other
 pub fn anagram(s1: &str, s2: &str) -> bool {
@@ -41,6 +43,50 @@ pub fn palindrome(s: &str) -> bool {
     s == s2
 }
 
+/// all permutations of a string
+pub fn permutations(s: &str) -> HashSet<String> {
+    // swap the first character with every other character in the string
+    // then ask for permutations for the remaining substring
+
+    // naive version with loads of string allocations
+    /*let mut v = HashSet::new();
+    let mut cs : Vec<char> = s.chars().collect();
+    v.insert(String::from(s));
+    for i in 0..cs.len() {
+        swap(&mut cs,0,i);
+        let ns = String::from_iter(cs.iter());
+        let s2 = permutations(&ns[1..]);
+        for mut ns in s2 {
+            ns.push(cs[0]);
+            v.insert(ns);
+        }
+        
+    }*/
+    // hopefully less allocations, we clone only the vector
+    let mut r = HashSet::new();
+    permute(s.chars().collect(),0,&mut r);
+    r.iter().map(|v| String::from_iter(v.iter())).collect()
+}
+
+/// internal permutation on character vector with start index
+fn permute(mut v: Vec<char>, st: usize, s: &mut HashSet<Vec<char>>) {
+    if st == v.len(){
+        s.insert(v);
+    } else {
+        for i in st..v.len() {
+            swap(&mut v,st,i);
+            permute(v.clone(), st+1, s);
+        }
+    }
+}
+
+/// swap two characters
+fn swap(nbs: &mut Vec<char>, low: usize, high: usize) {
+    let tmp: char = nbs[low];
+    nbs[low] = nbs[high];
+    nbs[high] = tmp;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -71,5 +117,11 @@ mod tests {
     fn test_palindrome(){
         assert!(palindrome("kayak"));
         assert_eq!(false,palindrome("hello"));
+    }
+
+    #[test]
+    fn test_permutations(){
+        let s= HashSet::from_iter(vec!("abc","bac","acb","cab","cba","bca").iter().map(|s| String::from(*s)));
+        assert_eq!(s,permutations("abc"));
     }
 }
