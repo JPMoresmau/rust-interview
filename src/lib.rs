da41bf61289f9070e2fab8300109e315ed1823c8
JP Moresmau <jp@moresmau.fr>
2019-06-22 15:09:56 +0200

    deranged anagrams

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,11 @@ pub mod array;
 pub mod llist;
 pub mod string;
 pub mod btree;
+pub mod deranged;
 
 
 pub use crate::array::*;
 pub use crate::llist::*;
 pub use crate::string::*;
 pub use crate::btree::*;
+pub use crate::deranged::*;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use interview::{get_words, longest_deranged};
+
 fn main() {
-    println!("Hello, world!");
+    let r = get_words();
+    match r {
+        Ok(mut v) => {
+            let od = longest_deranged(&mut v);
+            match od {
+                None => println!("No deranged anagrams found!"),
+                Some((s1,s2)) => println!("{} {}",s1,s2),
+            }
+        },
+        Err(e) => panic!("Could not read words: {}",e)
+    } 
 }
