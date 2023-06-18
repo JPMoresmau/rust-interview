b1178c298ce2fa481abbad3ddbeafd3af898ae79
JP Moresmau <jp@moresmau.fr>
2023-06-18 14:36:33 +0200

    increasing paths

--- src/matrix.rs
+++ src/matrix.rs
@@ -44,6 +44,58 @@ pub fn equal_pairs(grid: Vec<Vec<i32>>) -> i32 {
     eq
 }
 
+pub struct Solution {}
+
+impl Solution {
+    /// <https://leetcode.com/problems/number-of-increasing-paths-in-a-grid/>
+    const M: i64 = 1_000_000_007;
+
+    pub fn count_paths(grid: Vec<Vec<i32>>) -> i32 {
+        let m = grid.len();
+        let n = grid[0].len();
+        let mut dps = vec![vec![0_i64; n]; m];
+
+        for i in 0..m {
+            for j in 0..n {
+                Solution::dps(&grid, i, j, &mut dps);
+            }
+        }
+        dps.into_iter().fold(0, |cnt, v| {
+            v.into_iter().fold(cnt, |cnt2, v| (cnt2 + v) % Solution::M)
+        }) as i32
+    }
+
+    fn dps(grid: &Vec<Vec<i32>>, i: usize, j: usize, dps: &mut Vec<Vec<i64>>) -> i64 {
+        if dps[i][j] == 0 {
+            let v = grid[i][j];
+            let mut cnt = 1;
+            if i > 0 {
+                cnt += Solution::dps1(&grid, i - 1, j, dps, v);
+            }
+            if j > 0 {
+                cnt += Solution::dps1(&grid, i, j - 1, dps, v);
+            } 
+            if i < grid.len() - 1 {
+                cnt += Solution::dps1(&grid, i + 1, j, dps, v);
+            } 
+            if j < grid[0].len() - 1 {
+                cnt += Solution::dps1(&grid, i, j + 1, dps, v);
+            }
+            
+            dps[i][j] = cnt % Solution::M;
+        }
+        dps[i][j]
+    }
+
+    fn dps1(grid: &Vec<Vec<i32>>, i: usize, j: usize, dps: &mut Vec<Vec<i64>>, v: i32) -> i64 {
+        if grid[i][j] < v {
+            Solution::dps(&grid, i, j, dps)
+        } else {
+            0
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -77,5 +129,11 @@ mod tests {
                 vec![2, 4, 2, 2]
             ])
         );
+    }
+
+    #[test]
+    fn test_count_paths() {
+        assert_eq!(8, Solution::count_paths(vec![vec![1, 1], vec![3, 4]]));
+        assert_eq!(3, Solution::count_paths(vec![vec![1], vec![2]]));
     }
 }
